b92f97588b56a375804241c8f44f381830c84fb6
HyeonWoo <rmakxh@naver.com>
2023-02-01 12:23:32 +0900

    feat: Finish TodoInput component

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { DarkModeContext } from '../context/DarkModeProvider';
+import styles from '../styles/TodoInput.module.css';
+
+export default function TodoInput({ onSubmit }) {
+  const darkMode = useContext(DarkModeContext);
+
+  return (
+    <form
+      className={`${styles.todoInput} ${
+        darkMode.isDarkMode ? styles.dark : ''
+      }`}
+      onSubmit={onSubmit}
+    >
+      <input
+        type='text'
+        name='text'
+        className={styles.input}
+        placeholder='Add Todo'
+      />
+      <button type='submit' className={styles.add}>
+        Add
+      </button>
+    </form>
+  );
+}
